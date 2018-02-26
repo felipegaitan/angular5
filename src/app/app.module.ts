3196689230d2bbf0485f3706d8e31b436b3b5889
felipegaitan <felipe.gaitan.81@gmail.com>
2018-02-26 11:08:35 -0500

    server component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {  HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ServerComponentComponent } from './server-component/server-component.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponentComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
