ff1e0770e50d0405921c8eb34cf639e88aee3824
felipegaitan <felipe.gaitan.81@gmail.com>
2018-02-26 21:00:34 -0500

    propertie binding

--- src/app/server-component/server-component.component.ts
+++ src/app/server-component/server-component.component.ts
@@ -9,10 +9,15 @@ export class ServerComponentComponent implements OnInit {
 
   serverId: number;
   serverText: string;
+  allowNewServer: boolean;
 
   constructor() {
     this.serverId = 12;
+    this.allowNewServer = true;
     this.serverText = 'server down';
+    setTimeout(() => {
+      this.allowNewServer = false;
+    } , 2000 );
   }
 
   ngOnInit() {
