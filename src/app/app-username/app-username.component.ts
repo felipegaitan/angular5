ae2807493442d76ef2b5a21a1c29634649381f6b
felipegaitan <felipe.gaitan.81@gmail.com>
2018-02-28 10:25:26 -0500

    app-username component

---
+++ src/app/app-username/app-username.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-username',
+  templateUrl: './app-username.component.html',
+  styleUrls: ['./app-username.component.css']
+})
+export class AppUsernameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
